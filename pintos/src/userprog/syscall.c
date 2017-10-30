673cd8513cc3a925805616faf2e0fa02f48691f5
Aaron Xu <aaxu@berkeley.edu>
2017-10-30 13:18:35 +0000

    Return fd in syscall handler

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -4,6 +4,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "filesys/file.h"
+#include "threads/vaddr.h"
 
 
 static void syscall_handler (struct intr_frame *);
@@ -18,40 +19,47 @@ static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
   uint32_t* args = ((uint32_t*) f->esp);
+  check_memory_access(args, sizeof(args));
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
     printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
     thread_exit();
   } else if (args[0] == SYS_CREATE) {
     // Check memory accesses for all below
+    check_memory_access(args[1], sizeof((char *) args[1]));
     char *file_name = (char *) args[1];
     lock_filesys ();
     bool created = filesys_create (file_name, 1024); // Create new file with initial size;
     release_filesys ();
     f->eax = created;
   } else if (args[0] == SYS_REMOVE) {
+    check_memory_access(args[1], sizeof((char *) args[1]));
     char *file_name = (char *) args[1];
     lock_filesys ();
     bool removed = filesys_create (file_name); // Create new file with initial size;
     release_filesys ();
     f->eax = removed;
   } else if (args[0] == SYS_OPEN) {
+    // check_memory_access((char *) args[1], sizeof((char *) args[1])); // Why is this passing without checking memory?
     char *file_name = (char *) args[1];
     lock_filesys ();
     struct file *file = filesys_open (filesys_open); // Create new file with initial size;
+    int fd = insert_file_to_fd_table (file);
     release_filesys ();
-    f->eax = file;
+    f->eax = fd;
   } else if (args[0] == SYS_FILESIZE) {
     lock_filesys ();
     struct file *file = get_file (args[1]);
     f->eax = file_length (file);
     release_filesys ();
   } else if (args[0] == SYS_READ) {
+    check_memory_access(args[2], args[3]);
     lock_filesys ();
     struct file *file = get_file (args[1]);
     f->eax = file_read (file, args[2], args[3]);
     release_filesys ();
   } else if (args[0] == SYS_WRITE) {
+    check_memory_access(args[2], args[3]);
     lock_filesys ();
     if (args[1] == 1)
       {
@@ -78,5 +86,14 @@ syscall_handler (struct intr_frame *f UNUSED)
     struct file *file = get_file (args[1]);
     file_close (file);
     release_filesys ();
+  }
+}
+
+void
+check_memory_access (char *addr, int size) {
+  if (addr == NULL || !is_user_vaddr(addr + size)) {
+      // struct thread *current_thread = thread_current ();
+      // current_thread->info->exit_code = -1;
+      thread_exit ();
   }
 }
